cac9bfd90ea206040afd9ff4346169760e7241a3
tow <lgjy143@163.com>
2019-12-12 17:10:57 +0800

    init

---
+++ work/Admin/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Button} from 'antd';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Button type='primary'>测试按钮</Button>
+    </div>
+  );
+}
+
+export default App;
